5c39ffa1c6dc6ffc065249a37ec45270fb9f6eb2
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-09-29 20:00:23 +0300

    Добавьте файлы проекта.

---
+++ MD4/FileMD4.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+
+namespace MD4_hash
+{
+    /// <summary>Implements the MD4 message digest algorithm for hashing files</summary>
+    public class FileMD4 : MD4
+    {
+        public string? FileName
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                lastHash = null;
+            }
+        }
+
+
+        public FileMD4(string filename = null, string salt = "") : base(filename, salt) { }
+
+
+        /// <summary>
+        ///   Returns last hash value
+        /// </summary>
+        /// <returns>byte-array that contains the hash or null if nothing was hashed</returns>
+        override public byte[]? GetByteHash()
+        {
+            if (lastHash == null)
+                GetByteHash(Value);
+            return lastHash;
+        }
+
+        /// <summary>
+        ///   Returns a byte hash from a file
+        /// </summary>
+        /// <param name = "s">name of the file to hash</param>
+        /// <returns>byte-array that contains the hash</returns>
+        override public byte[] GetByteHash(string filename)
+        {
+            value = filename;
+
+            using FileStream fs = new FileStream(filename, FileMode.Open);
+            byte[] b = new byte[salt.Length + fs.Length];
+            fs.Read(b, salt.Length, (int)fs.Length);
+           
+            EngineReset();
+            EngineUpdate(b, 0, b.Length);
+            return lastHash = EngineDigest();
+        }
+
+        /// <summary>
+        ///   Returns a byte hash from a file
+        /// </summary>
+        /// <param name = "s">name of the file to hash</param>
+        /// <returns>byte-array that contains the hash</returns>
+        public Task<byte[]> GetByteHashAsync(string filename)
+        {
+            return Task.Run(() =>
+            {
+                Value = filename;
+                byte[] b = File.ReadAllBytes(filename);
+                EngineReset();
+                EngineUpdate(b, 0, b.Length);
+                return lastHash = EngineDigest();
+            });
+        }
+
+        /// <summary>
+        ///   Returns a byte hash from a file
+        /// </summary>
+        /// <returns>byte-array that contains the hash</returns>
+        public Task<byte[]> GetByteHashAsync()
+        {
+            return Task.Run(() =>
+            {
+                if (lastHash == null)
+                    lastHash = GetByteHash(Value);
+                return lastHash;
+            });
+        }
+    }
+}
---
+++ MD4/Utility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD4_hash
+{
+    public static class MD4Utility
+    {
+        public static string? ToHex(byte[] b, bool upperCase = false)
+        {
+            if (!upperCase)
+                return BitConverter.ToString(b).Replace("-", "").ToLower();
+            else
+                return BitConverter.ToString(b).Replace("-", "").ToUpper() ;
+        }
+    }
+}
