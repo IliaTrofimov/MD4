932669c23bc35e69df773a39ba61f93dc6538f40
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-10-01 00:08:13 +0300

    changed view models

---
+++ MD4_app/ViewModels/HashGeneratorViewModel.cs
@@ -0,0 +1,95 @@
+﻿using MD4_hash;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MD4_app.ViewModels
+{
+    public class HashGeneratorViewModel : BaseViewModel
+    {
+        private string salt = "";
+        private string hexHash = "";
+        private string? input;
+        private string? compareHash;
+        private bool isFileHasher = false;
+        private bool isEnabled = true;
+
+
+        public string Salt
+        {
+            get => salt;
+            set
+            {
+                salt = value;
+                OnPropertyChanged();
+            }
+        }
+        public string? Input
+        {
+            get => input;
+            set
+            {
+                input = value;
+                OnPropertyChanged();
+            }
+        }
+        public string? CompareHashHex
+        {
+            get => compareHash;
+            set
+            {
+                compareHash = value;
+                OnPropertyChanged();
+            }
+        }
+        public string HexHash
+        {
+            get => hexHash;
+            set
+            {
+                hexHash = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsFileHasher
+        {
+            get => isFileHasher;
+            set
+            {
+                isFileHasher = value;
+                OnPropertyChanged();
+                OnPropertyChanged("InputHorAligment");
+                OnPropertyChanged("InputVerAligment");
+                OnPropertyChanged("InputVeInputFontStylerAligment");
+                OnPropertyChanged("IsInputFieldEnabled");
+                OnPropertyChanged("IsStringHasher");
+            }
+        }
+        public bool IsEnabled
+        {
+            get => isEnabled;
+            set
+            {
+                isEnabled = value;
+                OnPropertyChanged();
+                OnPropertyChanged("IsInputFieldEnabled");
+                OnPropertyChanged("IsProgressing");
+            }
+        }
+
+
+        public ObservableCollection<HashHistoryItemViewModel> HistoryItems { get; set; } = new();
+
+
+        public bool IsStringHasher => !IsFileHasher;
+        public bool IsInputFieldEnabled => IsEnabled && !IsFileHasher;
+        public FontStyle InputFontStyle => IsFileHasher ? FontStyles.Italic : FontStyles.Normal;
+        public HorizontalAlignment InputHorAligment => IsFileHasher ? HorizontalAlignment.Center : HorizontalAlignment.Left;
+        public VerticalAlignment InputVerAligment => IsFileHasher ? VerticalAlignment.Center : VerticalAlignment.Top;
+        public bool IsProgressing => !IsEnabled;
+    }
+}
--- MD4_app/ViewModels/HashHistoryItemViewModel.cs
+++ MD4_app/ViewModels/HashHistoryItemViewModel.cs
@@ -5,20 +5,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 
 namespace MD4_app.ViewModels
 {
     public class HashHistoryItemViewModel : BaseViewModel
     {
-        public MD4 Hash;
+        private bool isComparing = false;
 
-        public HashHistoryItemViewModel(string value = null)
+
+        public MD4 Hash;
+        public bool IsComparing
         {
-            Hash = new(value);
+            get => isComparing;
+            set
+            {
+                isComparing = value;
+                OnPropertyChanged();
+                OnPropertyChanged("FontWeight");
+                OnPropertyChanged("FontColor");
+            }
         }
 
-        public string HashHex => Hash.GetHexHash();
+
+        public HashHistoryItemViewModel(MD4 hash)
+        {
+            Hash = hash;
+        }
+
+
+        public string HashHex => Hash.HexHash;
         public string? Value => Hash.Value;
-        public FontStyle FontStyle => Hash is FileMD4 ? FontStyles.Italic : FontStyles.Normal;
+        public string Label => Hash is FileMD4 ? "файл" : "строка";
+        public Color FontColor => isComparing ? Colors.CadetBlue : Colors.Black;
+        public FontWeight FontWeight => isComparing ? FontWeights.Bold : FontWeights.Normal;
     }
 }
