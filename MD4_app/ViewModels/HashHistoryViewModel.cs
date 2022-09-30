a38e91d14ab816a59babd62aa2f6ddf8968c60a1
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-10-01 00:07:58 +0300

    rem HashHistory VM

--- MD4_app/ViewModels/HashHistoryViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.ObjectModel;
-
-namespace MD4_app.ViewModels
-{
-    public class HashHistoryViewModel : BaseViewModel
-    {
-        public ObservableCollection<HashHistoryItemViewModel> HashesObservable { get; private set; } = new();
-    }
-}
