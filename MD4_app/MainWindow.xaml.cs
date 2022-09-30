dac66f3530187acfad36cd8220436c132001c93b
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-10-01 00:08:54 +0300

    fixed app logics

--- MD4_app/MainWindow.xaml.cs
+++ MD4_app/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MD4_app.ViewModels;
+using MD4_hash;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,62 @@ namespace MD4_app
     /// </summary>
     public partial class MainWindow : Window
     {
+        public HashGeneratorViewModel ViewModel { get; set; } = new();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel = new HashGeneratorViewModel();
+            DataContext = ViewModel;
+        }
+
+        private void SetFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new() { CheckFileExists = true };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ViewModel.IsFileHasher = true;
+                ViewModel.Input = openFileDialog.FileName;
+            }
+        }
+
+        private void Run_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.IsEnabled = false;
+            MD4 hasher = null;
+
+            Task.Run(() => {
+                hasher = ViewModel.IsFileHasher ? new FileMD4(ViewModel.Input, ViewModel.Salt) : new MD4(ViewModel.Input, ViewModel.Salt);
+                hasher.Calculate();
+                ViewModel.HexHash = hasher.HexHash;
+            
+            }).ContinueWith(t => Dispatcher.Invoke(() =>
+            {
+                ViewModel.IsEnabled = true;
+                if (hasher is not null) ViewModel.HistoryItems.Add(new(hasher));
+            }));
+        }
+
+        private void ToggleHistory_Click(object sender, RoutedEventArgs e)
+        {
+            if (groupBox_history.Visibility == Visibility.Collapsed)
+            {
+                menu_history.Header = "Скрыть историю";
+                groupBox_history.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                menu_history.Header = "История";
+                groupBox_history.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.HexHash = "";
+            ViewModel.Input = null;
+            ViewModel.IsFileHasher = false;
         }
     }
 }
