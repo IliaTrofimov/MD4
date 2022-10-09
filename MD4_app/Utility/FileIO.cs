7874944e77821ef1c9254fcc83cdad88b8bc7305
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-10-10 01:23:34 +0300

    add small utility classes

---
+++ MD4_app/Utility/FileIO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+
+namespace MD4_app.Utility
+{
+    public static class FileIO
+    {
+        public static string ReadHashFile(string filename)
+        {
+            return File.ReadAllText(filename);
+        }
+
+        public static void WriteHashFile(string hexhash, string filename)
+        {
+            File.WriteAllText(filename, hexhash);
+        }
+
+        public static string GenerateHashFileName(string value, bool isFileHash = false)
+        {
+            return !isFileHash ? $"hash_str_{value[0..Math.Min(value.Length, 30)]}_.txt" : $"hash_file_{Path.GetFileNameWithoutExtension(value)}_.txt";
+        }
+    }
+}
---
+++ MD4_app/Utility/PasswordValidationError.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MD4_app.Utility
+{
+    public enum PasswordValidationError
+    {
+        Ok, TooShort, TooLagre, RegexNotMatched
+    }
+
+    public static class PasswordValidation
+    {
+        public static PasswordValidationError Validate(string password)
+        {
+            if (Properties.Settings.Default.IsPasswordRequired)
+            {
+                int len = password.Length;
+                if (len > Properties.Settings.Default.PasswordMaxLength)
+                    return PasswordValidationError.TooLagre;
+                else if (len < Properties.Settings.Default.PasswordMinLength)
+                    return PasswordValidationError.TooShort;
+                if (Properties.Settings.Default.PasswordRegex != "" && !Regex.IsMatch(password, Properties.Settings.Default.PasswordRegex))
+                    return PasswordValidationError.RegexNotMatched;
+            }
+
+            return PasswordValidationError.Ok;
+        }
+    }
+
+}
